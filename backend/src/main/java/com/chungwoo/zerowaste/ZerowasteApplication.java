3d2728215b992b7ca091873b552c802cdfb51749
surhwa11228 <162419776+surhwa11228@users.noreply.github.com>
2025-07-26 22:40:07 +0900

    juan3355//init project

---
+++ backend/src/main/java/com/chungwoo/zerowaste/ZerowasteApplication.java
@@ -0,0 +1,13 @@
+package com.chungwoo.zerowaste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerowasteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZerowasteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/chungwoo/zerowaste/ZerowasteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chungwoo.zerowaste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerowasteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
