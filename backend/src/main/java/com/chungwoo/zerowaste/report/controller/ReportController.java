577007019335c3ccbfe91414cbbc798431b4d740
surhwa11228 <162419776+surhwa11228@users.noreply.github.com>
2025-07-26 22:40:07 +0900

    juan3355//report branch

---
+++ backend/src/main/java/com/chungwoo/zerowaste/report/controller/ReportController.java
@@ -0,0 +1,11 @@
+package com.chungwoo.zerowaste.report.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/report")
+public class ReportController {
+
+
+}
